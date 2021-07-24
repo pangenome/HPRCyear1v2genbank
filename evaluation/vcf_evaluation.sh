29187e7e615b1c13c368519f7f7606892ae76aa8
Erik Garrison <erik.garrison@gmail.com>
2021-07-24 10:24:13 +0000

    update eval scripts to only use vcfeval

--- evaluation/vcf_evaluation.sh
+++ evaluation/vcf_evaluation.sh
@@ -1,46 +1,44 @@
 #!/bin/bash
 
 # Variables
-REF=GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
-REF_SDF=GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.sdf
-TRUTH_VCF=HG00438.GRCh38_no_alt.deepvariant.vcf.gz
-
+REF=/lizardfs/erikg/HPRC/year1v2genbank/evaluation/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
+REF_SDF=/lizardfs/erikg/HPRC/year1v2genbank/evaluation/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.sdf
+TRUTH_VCF=/lizardfs/erikg/HPRC/year1v2genbank/evaluation/HG00438.GRCh38_no_alt.deepvariant.vcf.gz
 
 # Inputs
 SAMPLE=$1
 QUERY_VCF=$2
-REGIONS_BED=$3
-STRATIFICATION_TSV=$4
+EASY_REGIONS_BED=$3
+HARD_REGIONS_BED=$4
+#STRATIFICATION_TSV=$4
 OUTPUT_DIR=$5
 THREADS=$6
-
 
 echo "VCF renaming"
 zcat "$QUERY_VCF" | sed 's/^grch38#//g' | bgzip -c > "$QUERY_VCF".renamed.vcf.gz && tabix "$QUERY_VCF".renamed.vcf.gz
 
-
 echo "VCF processing"
-bash vcf_preprocess.sh "$QUERY_VCF".renamed.vcf.gz "$SAMPLE" 50
+bash /lizardfs/erikg/HPRC/year1v2genbank/evaluation/vcf_preprocess.sh "$QUERY_VCF".renamed.vcf.gz "$SAMPLE" 50
 rm "$QUERY_VCF".renamed.vcf.gz
 
-FNAME=$(basename "$QUERY_VCF".renamed.vcf.gz)
+FNAME=$(dirname "$QUERY_VCF")/$(basename "$QUERY_VCF".renamed.vcf.gz)
 PREFIX="${FNAME%.vcf.gz}"
 NORMALIZED_VCF=${PREFIX}.max50.chr1-22.vcf.gz
 
 echo "VCF evaluation"
-mkdir -p "$OUTPUT_DIR"
 
-OUTPUT_PREFIX="$OUTPUT_DIR"/"$SAMPLE"
+rtg vcfeval \
+    -t "$REF_SDF" \
+    -b "$TRUTH_VCF" \
+    -e "$EASY_REGIONS_BED" \
+    -c "$NORMALIZED_VCF" \
+    -T "$THREADS" \
+    -o "$OUTPUT_DIR".easy
 
-docker run -v "${PWD}"/:/data paramost/hap.py /opt/hap.py/bin/hap.py \
-     --threads "$THREADS"                             \
-     -r data/$REF                                     \
-     -o data/"$OUTPUT_PREFIX"                         \
-     -f data/"$REGIONS_BED"                           \
-     --stratification data/"$STRATIFICATION_TSV"      \
-     --no-leftshift                                   \
-     --no-decompose                                   \
-     --engine vcfeval                                 \
-     --engine-vcfeval-template data/$REF_SDF          \
-     data/$TRUTH_VCF                                  \
-     data/"$NORMALIZED_VCF"
+rtg vcfeval \
+    -t "$REF_SDF" \
+    -b "$TRUTH_VCF" \
+    -e "$HARD_REGIONS_BED" \
+    -c "$NORMALIZED_VCF" \
+    -T "$THREADS" \
+    -o "$OUTPUT_DIR".hard
--- evaluation/vcf_preprocess.sh
+++ evaluation/vcf_preprocess.sh
@@ -5,21 +5,22 @@
 
 VCF=$1
 FNAME=$(basename $VCF)
-PREFIX="${FNAME%.vcf.gz}"
+PREFIX=$(dirname $VCF)/"${FNAME%.vcf.gz}"
 SAMPLE=$2
 CHROMS="chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22"
 MAXSIZE=$3
-REF="GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
+REF="/lizardfs/erikg/HPRC/year1v2genbank/evaluation/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
 MEM="10G"
 
-bcftools view -a -s ${SAMPLE} -Ou ${VCF} \
-    | bcftools norm -f ${REF} -c s -m - -Ou \
-    | bcftools view -e 'GT="ref" | GT~"\."' -f 'PASS,.' -Ou \
-    | bcftools sort -m ${MEM} -T sort_tmp/ -Ou \
-    | bcftools norm -d exact -Oz -o ${PREFIX}.norm.vcf.gz \
-    && bcftools index -t ${PREFIX}.norm.vcf.gz \
-    && bcftools view -e "STRLEN(REF)>${MAXSIZE} | STRLEN(ALT)>${MAXSIZE}" \
-                     -r ${CHROMS} -Oz -o ${PREFIX}.max${MAXSIZE}.chr1-22.vcf.gz \
-                     ${PREFIX}.norm.vcf.gz \
-    && bcftools index -t ${PREFIX}.max${MAXSIZE}.chr1-22.vcf.gz \
+bcftools=/gnu/store/pr0v7dvbf16qkm45fiw26hspy77w5bb2-bcftools-1.12/bin/bcftools
+$bcftools view -a -s ${SAMPLE} -Ou ${VCF} \
+    | $bcftools norm -f ${REF} -c s -m - -Ou \
+    | $bcftools view -e 'GT="ref" | GT~"\."' -f 'PASS,.' -Ou \
+    | $bcftools sort -m ${MEM} -T bcftools-sort.XXXXXX -Ou \
+    | $bcftools norm -d exact -Oz -o ${PREFIX}.norm.vcf.gz \
+    && $bcftools index -t ${PREFIX}.norm.vcf.gz \
+    && $bcftools view -e "STRLEN(REF)>${MAXSIZE} | STRLEN(ALT)>${MAXSIZE}" \
+                 -r ${CHROMS} -Oz -o ${PREFIX}.max${MAXSIZE}.chr1-22.vcf.gz \
+                 ${PREFIX}.norm.vcf.gz \
+    && $bcftools index -t ${PREFIX}.max${MAXSIZE}.chr1-22.vcf.gz \
     && rm ${PREFIX}.norm.vcf.gz*
